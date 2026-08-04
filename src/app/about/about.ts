4ed753c95c50d849884f0cf8278e6f2bdebc9d30
Andrew Marshall <andrewleonmarshall@gmail.com>
2026-08-04 18:46:50 -0400

    updated about page content

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 import { AboutApproach } from './approach/approach';
 import { AboutCta } from './cta/cta';
-import { FindMe } from './find-me/find-me';
 import { AboutHero } from './hero/hero';
 import { ProfessionalBackground } from './professional-background/professional-background';
 import { WhoIHelp } from './who-i-help/who-i-help';
@@ -10,15 +9,7 @@ import { WhatToExpect } from './what-to-expect/what-to-expect';
 
 @Component({
   selector: 'app-about',
-  imports: [
-    AboutHero,
-    ProfessionalBackground,
-    AboutApproach,
-    WhoIHelp,
-    WhatToExpect,
-    FindMe,
-    AboutCta,
-  ],
+  imports: [AboutHero, ProfessionalBackground, AboutApproach, WhoIHelp, WhatToExpect, AboutCta],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
